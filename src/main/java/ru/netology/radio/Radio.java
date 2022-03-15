c46330dd9326fdd9ad410e785afb4fa7e0b3a62c
nanaishere <naiavasilyeva@gmail.com>
2022-03-15 12:35:27 +0300

    changed files

--- src/main/java/ru/netology/radio/Radio.java
+++ src/main/java/ru/netology/radio/Radio.java
@@ -2,6 +2,25 @@ package ru.netology.radio;
 
 public class Radio {
     private int currentRadioStationNumber;
+    private int currentVolume;
+    private int numberRadioStations = 10;
+    private int maxNumber = numberRadioStations - 1;
+
+    public Radio() {
+    }
+
+    public Radio(int numberRadioStations) {
+        this.numberRadioStations = numberRadioStations - 1;
+        this.maxNumber = numberRadioStations - 1;
+    }
+
+    public int getNumberRadioStations() {
+        return numberRadioStations;
+    }
+
+    public void setNumberRadioStations(int numberRadioStations) {
+        this.numberRadioStations = numberRadioStations;
+    }
 
     public int getCurrentRadioStationNumber() {
         return currentRadioStationNumber;
@@ -17,26 +36,24 @@ public class Radio {
 
     public void setCurrentRadioStationNumber(int currentRadioStationNumber) {
         if (currentRadioStationNumber < 0) {
-            currentRadioStationNumber = 9;
+            currentRadioStationNumber = maxNumber;
         }
-        if (currentRadioStationNumber > 9) {
+        if (currentRadioStationNumber > maxNumber) {
             currentRadioStationNumber = 0;
         }
         this.currentRadioStationNumber = currentRadioStationNumber;
     }
-
-    private int currentVolume;
 
     public int getCurrentVolume() {
         return currentVolume;
     }
 
     public void setCurrentVolume(int currentVolume) {
-        if (currentVolume < 0) {
-            return;
+        if (currentVolume <= 0) {
+            currentVolume = 0;
         }
-        if (currentVolume > 10) {
-            return;
+        if (currentVolume > 100) {
+            currentVolume = 100;
         }
         this.currentVolume = currentVolume;
     }
